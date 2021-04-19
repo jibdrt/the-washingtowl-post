bf7006a775d15bc368527ad631d9ac605b2dd729
williams <w.chazal@codeur.online>
2021-04-19 16:52:14 +0200

    flip-card

---
+++ flip-card/filp-card.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flip-card</title>
+    <link rel="stylesheet" href="flip-card.css"></link>
+</head>
+<body>
+
+<div class="card">
+    <div class="card__inner">
+        <div class="card__face card__face--front">
+            <div class="container-white">
+                <div class="card__header--front"><h2> La Cryptographie </h2>
+                    <div class="container">              
+                    <h4>"La cryptographie c'est l'art de savoir être secret"</h4>                                    
+                 </div>
+             </div>
+            </div>
+        </div>
+            
+            <div class="card__face card__face--back">
+                    <div class="card__header--back"><h2> La Cryptographie </h2>   
+                    </div>
+                    <br>
+                    <div class="card__body--back">
+                            <img src="cyber-security.jpg" alt="" class="img" /></div>
+                   <br>
+                    <div class="card__footer--back">
+                        <button>Lire l'article</button>
+                    </div>
+            </div>
+    </div>
+</div>
+
+ <script src="flip-card.js"></script>   
+</body>
+</html>
