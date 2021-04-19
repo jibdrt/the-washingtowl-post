3d725eeabe8271e70aad2d3b166172e5f46c92cd
JbDruet <jb.druet@codeur.online>
2021-04-19 16:57:58 +0200

    headerarticle

---
+++ niv3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>pagearticle</title>
+</head>
+<body>
+    <div class="container head">
+            <div class="row">
+                <div class="col-md-2 col-sm-12 text-white" id="cybersecu">Cybersécurité</div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-sm-12 text-white mt-2 mb-2" id="articleTitle">La cryptographie ou l'art du secret</div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-sm-12" id="keyWords">CHIFFREMENT-CRYPTOSYSTEME-CRYPTANALYSE-CRYPTOLOGIE</div>
+            </div>
+    </div>
+    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6" id="bloc_1"><img src="" alt=""></div>
+                <div class="col-md-5" id="glossaire" style="color:#FFFF">GLOSSAIRE
+                    <div class="col-md-8 mb-3 mt-3" id="glossaireContent" style="color:#333">CHIFFREMENT: remplacer les caractère d'un texte "en clair" par d'autre pour le rendre illisible.
+CRYPTOSYSTEME: algoritme de chiffrement.
+CRYPTANALYSE: science de l'étude des messages chiffrées en vue de leur déchiffrement.
+CRYPTOLOGIE: science regroupant la cryptographie et la cryptanalyse.<div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
