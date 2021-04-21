adb5ef479495bbdd89b44a14ce3ca4074aeab894
JbDruet <jb.druet@codeur.online>
2021-04-21 09:05:00 +0200

    digitaljobs

--- niv3/index.php
+++ niv3/index.php
@@ -9,30 +9,52 @@
     <title>pagearticle</title>
 </head>
 <body>
-    <div class="container-fluid">
+    <div class="container">
             <div class="row">
-        
-                <div class="col-md-2 col-sm-10 text-white" id="cybersecu">Cybersécurité</div>
+                <div class="col-md-3 col-sm-8 text-white" id="cybersecu">Cybersécurité</div>
             </div>
             <div class="row">
-                <div class="col-md-10 col-sm-12 text-white mt-2 mb-2" id="articleTitle">La cryptographie ou l'art <br>du secret</div>
+                <div class="col-md-10 col-sm-12 text-white mt-2 mb-3" id="articleTitle">La cryptographie ou l'art du secret</div>
             </div>
             <div class="row">
-                <div class="col-md-10 col-sm-12" id="keyWords">CHIFFREMENT-CRYPTOSYSTEME-CRYPTANALYSE-CRYPTOLOGIE</div>
+                <div class="col-md-10 col-sm-12 mb-4" id="keyWords">CHIFFREMENT-CRYPTOSYSTEME-CRYPTANALYSE-CRYPTOLOGIE</div>
             </div>
     </div>
-    <div class="container-fluid">
+    
         <div class="container">
-            <div class="row">
-                <div class="col-md-6" id="bloc_1"><img src="" alt=""></div>
-                <div class="col-md-5" id="glossaire" style="color:#FFFF">GLOSSAIRE
-                    <div class="col-md-8 mb-3 mt-3" id="glossaireContent" style="color:#333">CHIFFREMENT: remplacer les caractère d'un texte "en clair" par d'autre pour le rendre illisible.
-CRYPTOSYSTEME: algoritme de chiffrement.
-CRYPTANALYSE: science de l'étude des messages chiffrées en vue de leur déchiffrement.
-CRYPTOLOGIE: science regroupant la cryptographie et la cryptanalyse.<div>
+            <div class="row pb-3">
+                <div class="col-md-6 col-sm-12 me-2" id="bloc_1"><img src="dictio.jpg" alt="" class="col-sm-12" id="firstImage"></div>
+                <div class="col-md-5 pb-4 ps-3" id="glossaire" style="color:#FFFF"><p class="ps-5">GLOSSAIRE</p>
+                    <div class="col-md-9 mx-auto" id="glossaireContent" style="color:#333">CHIFFREMENT: remplacer les caractère d'un texte "en clair" par d'autre pour le rendre illisible.<br>
+                    CRYPTOSYSTEME: algoritme de chiffrement.<br>
+                    CRYPTANALYSE: science de l'étude des messages chiffrées en vue de leur                  déchiffrement.<br>
+                    CRYPTOLOGIE: science regroupant la cryptographie et la cryptanalyse.<div>
                 </div>
             </div>
         </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-sm-12 ms-5 ps-5">
+            <h2 class="text-white m-3 p-3 w-100" id="cryptoNumeri">La cryptographie dans le <br>numérique</h2>
+
+
+                    <div class="row">
+                            <div class="col-md-12 col-sm-12 ms-4 p-4" id="cryptoNumeriTxt">Dans le numérique, la cryptographie est omni-présente. D'abord utilisé dans les communications militaires, puis dans les messages relevants du secret d'état en général (sphère diplomatique,...), elle s'est continuellement perfectionné et démocratisé. En effet, les secteurs bancaires et industriels ont rapidement compris que la posession de technologie fiables serait vital, pour sécurisé les informations bancaires et contrer l'espionnage industriel. Aujourd'hui, sur internet, le simple fait de rentrer sur un espace personel demande par exemple:
+                            </div>
+                    </div>
+
+                    <div class="row">
+                            <div class="col-md-12 col-sm-12 ms-4 p-4 m-3" id="cryptoNumeriTxt1">- un procédé cryptographique de certification du dommaine (pour s'assurer que l'on ne se trouve pas sur un faux site destinné à voler notre mot de passe via le formulaire d'entrer par exemple)
+                            </div>
+                    </div>
+
+
+            </div>
+        <div class="col-md-5 col-sm-12"></div>
     </div>
+    
+</div>
+   
 </body>
 </html>
