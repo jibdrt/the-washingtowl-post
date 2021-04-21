269260331337c5e8af7b726fba1857405ac72079
williams <w.chazal@codeur.online>
2021-04-21 17:09:58 +0200

    update du 21/04 avec les 2 flip-cards

---
+++ flip-card-2/flip-card-2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flip-card-2</title>
+    <link rel="stylesheet" href="../flip-card-2/flip-card-2.css"></link>
+</head>
+<body>
+
+<div class="card">
+    <div class="card__inner-2">
+        <div class="card__face card__face--front">
+        <div class="card__header--front"><h2> Pentesteur, le Corsaire des Hackers </h2></div>
+            <div class="container-white-2">
+                    <div class="container-hacking">              
+                    <span><h4>Le métier de « Pentesteur » peut aussi être assimilé à du « Ethical hacking »</h4></span>                                    
+                 </div>
+                
+            </div>
+          
+        </div>
+            
+            <div class="card__face card__face--back">
+               <div class="card__content">
+                   <div class="card__header--back"><h2> Pentesteur, le Corsaire des Hackers </h2></div>
+                   <div class="card__body--back"><img src="../flip-card-2/computer.svg" alt="" class="img"></div>
+                   <div class="card__footer--back"><button>Lire l'article</button></div>
+               </div> 
+            </div>   
+    </div>
+</div>
+
+
+ <script src="../flip-card-2/flip-card-2.js"></script>   
+</body>
+</html>
--- niv2/niv2.php
+++ niv2/niv2.php
@@ -10,40 +10,45 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-12 col-md-10 m-auto mt-2">
+        <div class="col-sm-12 col-md-10 m-auto mt-4">
         <h3>Protégez vous Cyber-ressources et données critiques</h3>
         </div>
     </div>
-   
-        <div class="row">
-        
-                <div class="col-sm-12 col-md-1"></div>
+<div class="container ">
+        <div class="row mt-5 bckgrd-color">
+                <div class="col-sm-12 col-md-5 pt-3"><p id="p"> La cybersécurité assure une gestion de la data dans des conditions optimales et sécurisées. Elle permet la protection des systèmes d’informations et des données qui circulent contre ceux que l’on appelle les cybercriminels. Les compétences en informatiques acquises par les personnes malveillantes sont des risques à ne pas prendre à la légère. De l’installation d’un antivirus jusqu’à la configuration de serveurs, ou encore le gardiennage des datas centers et des bureaux, la sécurité informatique impacte tous les métiers.</p>
+                </div>  
                 
-                <div class="col-sm-12 col-md-5 pt-3"><p> La cybersécurité assure une gestion de la data dans des conditions optimales et sécurisées. Elle permet la protection des systèmes d’informations et des données qui circulent contre ceux que l’on appelle les cybercriminels. Les compétences en informatiques acquises par les personnes malveillantes sont des risques à ne pas prendre à la légère. De l’installation d’un antivirus jusqu’à la configuration de serveurs, ou encore le gardiennage des datas centers et des bureaux, la sécurité informatique impacte tous les métiers.</p>
-                </div> 
-                
-                <div class="col-sm-12 col-md-6 me-auto"><img src="../assets/image1-niv2.svg" class="image-size" >
+                <div class="col-sm-12 col-md-7 mt-3" id="img-position"><img src="../assets/image1-niv2.svg" id="image-size" >
                 </div>
-                
-               
-         </div>
-    
-         <div class="row">
-             <div class="col-sm-12 col-md-6"></div>
+                </div>
+                 <div class="row pt-5 ms-0 blue-line"></div>             
+     </div>
+
+         <div class="row bckgrd-color-2">
              <div class="col-sm-12 col-md-6">
-             <?php include "../flip-card/filp-card.php";?>
+                 <div class="row" id="row-zizi">
+                 <?php include "../flip-card/filp-card.php";?>
+                 </div>
              </div>
-         </div>
+             <div class="col-sm-12 col-md-6" id="row-card-2"><?php include "../flip-card-2/flip-card-2.php";?>
+            </div>
+         </div> 
+</div>  
+<div class="container">
+<div class="row">
+    <div class="col-sm-12 col-md-6 mt-5">
+    <img src="../niv2/data.svg" id="img-size-2" >
+    </div>
+    <div class="col-sm-12 col-md-6 ">
 
-
-
-
-    
-   
-
-   
-   
-
-
+        <div class="row mt-5"></div>
+        <div class="row mt-5 bg-dark"> <p id="p2"> La cybersécurité assure une gestion de la data dans des conditions optimales et sécurisées. Elle permet la protection des systèmes d’informations et des données qui circulent contre ceux que l’on appelle les cybercriminels. Les compétences en informatiques acquises par les personnes malveillantes sont des risques à ne pas prendre à la légère. De l’installation d’un antivirus jusqu’à la configuration de serveurs, ou encore le gardiennage des datas centers et des bureaux, la sécurité informatique impacte tous les métiers.</p>
+                </div>  </div>
+        <div class="row mt-5"></div>
+    </div>
 </div>
+</div>
+</div>
+
 </body>
