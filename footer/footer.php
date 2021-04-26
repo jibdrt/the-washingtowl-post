d761383ecff29dff0fa5a93c693600a7872e87ac
williams <w.chazal@codeur.online>
2021-04-26 17:14:17 +0200

    footer responsive -- création page mention légales

--- footer/footer.php
+++ footer/footer.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>footer</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="../footer/footer.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
 </head>
 <body class="bg-dark">
   <div class="container">
-  <div class="row footer__background-color footer_font-color">
-        <div class="col-sm-12 col-md-12 ">
-        <span id="footer__haut-page">Haut de la page</span>
-        <span id="footer__titre"> <img src="../footer/GOODV.svg">The   Washingt' Owl  Post</span>
-        <span id="footer__mentions">Mentions légales</span>
-        </div>
-   </div>
-   </div>
+          <div class="row footer__background-color footer_font-color">
+                  <div class="col-sm-12 col-md-2 footer__haut-page">
+                      Haut de la page</div>
+                  <div class="col-sm-12 col-md-6 footer__titre me-5"><img src="../footer/GOODV.svg">The   Washingt' Owl  Post</div>
+                  <div class="col-sm-12 col-md-2 footer__mentions ">Mentions légales</div>        
+           </div>
+    </div>
+      
     
 
 </body>
