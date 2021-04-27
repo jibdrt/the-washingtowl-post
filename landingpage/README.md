c79b30c921e88df1c0b5a8b67e0bb3f34bfc62a8
JbDruet <jb.druet@codeur.online>
2021-04-27 14:59:12 +0200

    landingtest

---
+++ landingpage/README.md
@@ -0,0 +1,2 @@
+# Ecocabins
+- Intégration d'une maquette conçue par un webdesigner professionnel téléchargée sur Figma : "https://www.figma.com/file/1rOOkIkJowxEceOVjkMrvX/EcoCabins-landingspage?node-id=0%3A1"
---
+++ landingpage/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Intégration d'une maquette conçue par un webdesigner professionnel téléchargée sur Figma.">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="ecocabsquare.png" type="image/png">
+    <title>Ecocabins</title>
+</head>
+<body>
+
+
+<div class="container">
+
+
+
+<div id="section-1">
+        
+
+<div class="row justify-content-end">
+              <div id="social-network" class="col-sm-1">
+                <span class="social-network-pointer">Instagram</span> 
+                <span class="social-network-pointer">Facebook</span>
+                <span class="social-network-pointer">Linkedin</span>   
+              </div>
+        </div>
+
+<div class="container">
+
+
+
+   <!-- début de la nav -->
+   <nav class="navbar navbar-expand-lg navbar-dark container-fluid pb-5 pt-4">
+  
+    <a class="navbar-brand" href="#">
+        <img src="EcoCabins-white.png" id="logo" alt="le logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <span class="me-auto"></span> <!-- pour le positionnement à droite des liens -->
+      <span class="navbar-text">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item me-5">
+          <a class="nav-link active" aria-current="page" href="#">Modellen</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="#">Kopen</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="#">Huren</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="#">Nieuws</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-contact" id="contactbuton" href="#">Contact</a>
+        </li>
+      </ul>
+      </span>
+    </div>
+  
+</nav>
+<!-- fin de la nav -->
+
+  <div class="row mt-5">
+    <div class="col-sm mt-5">
+    <div class="mt-5 pt-5">
+      <p id="title-color-white">
+          Duurzame tiny huisjes
+        </p>
+          <p id="text-color-white">
+              <strong>Een ecologisch tiny huisje is<br> zoveel mogelijk opgebouwd<br> uit duurzame natuurlijke<br> materialen</strong>
+            </p>
+    </div>
+    </div>
+    <div class="col-sm">
+      <div class="bdf">
+    <p id="text-color-white2"><img src="btnplay.png" id="playbuton">Bekjik de film</p>
+</div>
+      
+    </div>
+  </div>
+</div>
+</div>
+<div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h2 id="duurzame">Duurzame materialen</h2>
+        <p id="alleecocabins">
+          Alle EcoCabins hebben een hoge isolatiewaarde, worden<br> geproduceerd met indien mogelijk duurzame en/of<br> natuurlijke materialen en hebben als opvallende uiterlijke<br> kenmerk de ‘ronde hoeken', mogelijk gemaakt door het <br>specifieke materiaalgebruik. De voordelen van bouwen met hout zijn legio: hout is licht, flexibel, isoleert goed en bouwt snel en efficient.</p>
+          <button class="lees-meer" type="button">
+          Lees meer
+          </button>
+</div>
+      <div class="col-md-6"><img src="Poutres.png" id="poutre"></div>
+</div>
+
+
+</div>
+</div>
+</div>
+<div class="container">
+      <div class="row">
+          <div class="col-sm-12 col-md-12 .bg-dark"></div>
+</div>
+
+
+</div>
+
+<div class="container" id="bg-test">
+<div class="row row-sm-12 pt-5 mt-5 mr-ml-5">
+    <div class="col-sm-12 col-md-4 text-white pb-5 pt-2">
+    <div class="row pb-5" id="innovatie">
+      <h1>Innovatie</h1></div>
+      <div class="textinnovatie pb-5">Wij hebben als doel om de EcoCabins voortdurend verder te ontwikkelen en innoveren, zodat de EcoCabins uiteindelijk 100% duurzaam en circulair zijn. Voorwaarde hierbij is dat de EcoCabins betaalbaar blijven. Met behulp van de zon, de wind en de beste isolatie geniet u van.</div>
+    </div>
+
+
+
+
+
+    <div class="col-sm-12 col-md-4 text-white">
+      <div class="textinnovatie pt-1">Alle EcoCabins hebben een hoge isolatiewaarde, worden geproduceerd met indien mogelijk duurzame en/of natuurlijke materialen en hebben als opvallende uiterlijke kenmerk de ‘ronde hoeken’, mogelijk gemaakt door het specifieke materiaalgebruik De voordelen van bouwen.</div>
+    </div>
+  </div>
+
+
+
+   
+    
+
+  </div>
+  
+
+
+
+  </div>
+
+
+
+<div class="container pt-5 mt-5">
+  <div class="row pt-4 ps-3 pb-5 mb-5">
+    <div class="col-sm-12 col-md-6 ps-5">
+      <div class="ps-5 ms-4"><img src="cabpage.png" id="cabpaged" ></div>
+    </div>
+    <div class="col-sm-12 col-md-6">
+      <div class="row pt-4">
+      <div class="col-sm-12 col-md-5">
+        <h2>Hout</h2>
+        <p>De draagconstructies van EcoCabins bestaan uit hout. Hout is duurzaam en.</p>
+      </div>
+      <div class="col-sm-12 col-md-5">
+        <h2>Glaswol isolatie</h2>
+        <p>De combinatie van glaswol met een houtskelet constructie geeft een.</p>
+      </div>
+      <div class="col-sm-12 col-md-5">
+        <h2>Afwerking</h2>
+        <p>De draagconstructies van EcoCabins bestaan uit hout. Hout is duurzaam en.</p>
+      </div>
+      <div class="col-sm-12 col-md-5">
+      <h2>Triple gas</h2>
+      <p>De combinatie van glaswol met een houtskelet constructie geeft een.</p>
+      </div>
+      <div class="col-sm-12 col-md-5">
+       <h2> Stalen onderstel</h2>
+       <p>Het stalen onderstel is sterk zijn en heeft de benodigde stijfheid die.</p>
+      </div>
+      <div class="col-sm-12 col-md-5">
+        <h2>Infrarood vloer</h2>
+        <p>De vloer wordt opgewarmd middels opgewekte warmte uit eigen zonne-energie.</p>
+      </div>
+        </div>
+      </div>
+              
+
+
+
+
+
+
+
+
+
+  </div>
+</div>
+
+<!-- row de fin de page -->
+<div class="container" id="bg-test">
+<div class="row text-white pb-3 pt-3">
+    <div class="col-sm-12 col-md-3 pt-3" id="infocontact">
+    info@eco-cabins.com
+    </div>
+    <div class="col-sm-12 col-md-4 pt-3 ps-3">
+    <img src="EcoCabins-white.png" id="logofooter">
+    </div>
+    <div class="col-sm-12 col-md-3 text-end ms-5 pt-3" id="cookiesplus">
+    Cookies Privacybeleid
+    </div>
+  </div>
+</div>
+</div>
+<!-- row de fin de page -->
+
+</body>
