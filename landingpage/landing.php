6fce905069e467723ef741aeef85d2686a7cb6f8
JbDruet <jb.druet@codeur.online>
2021-06-10 12:57:17 +0200

    update

---
+++ landingpage/landing.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<?php include "../header/header.php";?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="landing.css">
+    <title>Accueil</title>
+</head>
+<body>
+    
+<div class="wrapper">
+
+<div class="space">ok</div>
+
+<div id="bg">
+                        <span class="mdw"><p></p></span>
+                        <h1 ></h1>
+</div>
+
+<div class="space">ok</div>
+
+</div>
+
+<div class="wrapper1">
+
+    <div class="space">ok</div>
+        <div class="bg1">
+        <div></div>
+        <div><img src="pic2.jpg" alt="" id="painting"></div>
+        <div></div>
+        </div>
+    <div class="space">ok</div>
+
+
+</div>
+
+
+
+
+<div class="wrapper2">
+
+    <div class="space">ok</div>
+        <div class="bg2">
+
+
+        </div>
+    <div class="space">ok</div>
+
+
+</div>
+
+
+
+
+
+</body>
+<?php include "../footer/footer.php";?>
+</html>
---
+++ landingpage/landingtest.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Intégration d'une maquette conçue par un webdesigner professionnel téléchargée sur Figma.">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="landingtest.css">
+    <link rel="shortcut icon" href="../assets/GOODV.svg" type="image/svg">
+    
+    <title>Page d'accueil</title>
+</head>
+
+
+
+<div class="container bg-white">
+
+<?php include "../header/header.php";?>
+
+
+<div id="section-1">
+        
+
+<div class="row justify-content-end">
+              <div id="social-network" class="col-sm-1">
+                <a href="https://github.com/jibdrt"
+                target="_blank">
+                <span class="social-network-pointer text-white"><img class="logosn" id="logo1" src="github.svg" alt=""></span></a>
+                <a href="https://www.linkedin.com/in/jean-baptiste-druet-838593144" target="_blank">
+                <span class="social-network-pointer text-white"><img class="logosn" src="linkedin.svg" alt=""></span></a>
+                <a href="https://github.com/WilliamsChazal" target="_blank">
+                <span class="social-network-pointer text-white"><img class="logosn" src="github.svg" alt=""></span></a>
+                <a href="https://www.linkedin.com/in/williams-chazal-0a05a313a/" target="_blank">
+                <span class="social-network-pointer text-white"><img class="logosn" src="linkedin.svg" alt=""></span></a>
+              </div>
+        </div>
+
+<div class="container">
+
+
+
+  
+<!-- fin de la nav -->
+
+  <div class="row mt-5">
+    <div class="col-sm mt-5">
+    <div class="mt-5 pt-5">
+      <p id="title-color-white">
+          Les métier du web
+        </p>
+          <p id="text-color-white">
+              <strong>Les métiers du développement web sont en pleine expansion ...</strong>
+            </p>
+    </div>
+    </div>
+    <div class="col-sm">
+      <div class="bdf">
+    <p id="text-color-white2"><a href="../les-metiers/index.php"><button id="playbuton">Lire l'article</button></a></p>
+</div>
+      
+    </div>
+  </div>
+</div>
+</div>
+<div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h2 id="cybersecumetier">La cybersécurité au sein des métiers du numérique</h2>
+        <p id="cybersecu__txt">
+        La cybersécurité assure une gestion de la data dans des conditions optimales et sécurisées. Elle permet la protection des systèmes d’informations et des données qui circulent contre ceux que l’on appelle les cybercriminels.
+</p>
+          <a href="../cybersecurite/cybersecurite.php"><button class="lees-meer" type="button">
+          Lire l'article
+          </button></a>
+</div>
+      <div class="col-md-6"><img src="pic2.jpg" id="poutre"></div>
+</div>
+
+
+
+<div class="container">
+      <div class="row">
+          <div class="col-sm-12 col-md-12"></div>
+</div>
+
+
+</div>
+
+<div class="container" id="bg-test">
+<div class="row row-sm-12 pt-5 mt-5 mr-ml-5">
+    <div class="col-sm-12 col-md-4 text-white pb-5 pt-2">
+        
+    </div>
+
+
+
+
+
+    <div class="col-sm-12 col-md-4 text-white">
+      
+      <div class="row pb-1 mt-5" id="webdesigner">
+      <h1>Web designer , qu'est ce que c'est ?</h1></div>
+      <div class="webdesigner__txt pb-5"><p>"Dans les entreprises plus spécialisées, le webdesigner travaillera en équipe, en collaboration étroite avec un chef de projet et/ou des responsables artistiques et éditoriaux qui guideront et valideront son travail au fur et à mesure de l’avancement du projet ..."<p><a href="../articledesignerweb/index.php">
+      <button id="webdesigner__btn">Lire l'article</button></a>
+      </div>
+
+    </div>
+
+  </div>
+
+
+   
+    
+
+ 
+
+
+ 
+
+
+
+</body>
+<?php include "../footer/footer.php";?>
